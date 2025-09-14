42009cc68efedf9b5febc4a85fed4ca0319d76f3
mkacm27 <mkacm27@gmail.com>
2025-09-14 23:01:42 +0100

    fix: add entrypoint and downgrade libraries

---
+++ index.js
@@ -0,0 +1,7 @@
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+
+// The app name is defined in the 'name' field of your package.json
+const appName = 'class-print-flow-native';
+
+AppRegistry.registerComponent(appName, () => App);
