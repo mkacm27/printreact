7a7f8a2b08e816547781af4ac94cdc8fa830d4f5
mkacm27 <mkacm27@gmail.com>
2025-09-17 16:31:54 +0100

    fix(metro): blacklist duplicate assets

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 /**
  * Metro configuration
@@ -6,6 +7,13 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+    resolver: {
+        blacklistRE: exclusionList([
+            /.*\/node_modules\/react-native-calendars\/src\/img\/.*/,
+            /.*\/node_modules\/react-navigation-elements\/src\/assets\/.*/,
+        ])
+    }
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
